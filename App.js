4b0c01667bf0daaea2f6f22387cdcd3cb466c0cb
Tyler Fry <fry.tyler@gmail.com>
2017-10-30 09:35:57 -0400

    feat(StyledComponents): Add Styled Components Package
    #4

--- App.js
+++ App.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import styled from 'styled-components/native';
+
+const StyledText = styled.Text`
+  color: red;
+`;
 
 export default class App extends React.Component {
   render() {
@@ -7,7 +12,7 @@ export default class App extends React.Component {
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <StyledText>Shake your phone to open the developer menu.</StyledText>
       </View>
     );
   }
