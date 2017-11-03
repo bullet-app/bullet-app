238d201d3138312db774cc2e214722a179a6be7f
Eric Adamski <er.adamski@gmail.com>
2017-11-03 09:36:49 -0400

    refactor(Title): Update fonts to use imported fonts

--- src/components/title/title.styled.js
+++ src/components/title/title.styled.js
@@ -6,6 +6,7 @@ export const Main = styled.View``;
 
 export const Text = styled.Text`
   color: ${colors.text};
+  font-family: ${font.family.bold};
   font-size: ${props =>
-    props.secondary ? font.title.base : font.title.base * 2}px;
+    props.secondary ? font.title.base : font.title.base * 1.5}px;
 `;
--- src/utils/font.js
+++ src/utils/font.js
@@ -1,4 +1,10 @@
 export const font = {
+  family: {
+    regular: 'montserrat-regular',
+    medium: 'montserrat-medium',
+    semibold: 'montserrat-semi-bold',
+    bold: 'montserrat-bold',
+  },
   base: 14,
   title: {
     base: 18,
